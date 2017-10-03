111880b40cc63609f32183b4ca3579ebdad2586d
Han <ghan1@DOCMM0CL5029265.local>
2017-10-03 17:43:09 -0500

    in case the file path has spaces...

--- map_go.sh
+++ map_go.sh
@@ -1,11 +1,11 @@
 ##the function which call map and cd to change the wd of the invoking terminal
 function g {
     if (($# == 0)); then
-	echo \"Usage: g pathmarker\"
-	return
+        echo \"Usage: g pathmarker\"
+        return
     fi
     path=`map $1`
     if [ ! -z "$path" ];then
-	cd $path 
+        cd "$path"  ##quoted in case the file path has whitespaces
     fi
 } 
